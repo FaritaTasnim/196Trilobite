00b9b6b038b613f8beae99d55c3482542e5f0e1b
FaritaTasnim <farita@mit.edu>
2022-12-30 08:38:09 -0700

    apex cname

--- push.py
+++ push.py
@@ -3,7 +3,7 @@
 import os, sys
 
 os.system('git pull origin master')
-os.system('git add --all')
+os.system('git add .')
 os.system('git commit -m "' + " ".join(sys.argv[1:]) + '"')
 os.system('git push -u origin master')
 
